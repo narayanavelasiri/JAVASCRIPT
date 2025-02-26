8a3675bfed40d2c9cee37be5555efe6fe1bd261a
narayanavelasiri <narayanavelasiri@gmail.com>
2025-02-26 19:38:34 +0530

    domappjs

---
+++ app.js
@@ -0,0 +1,29 @@
+// Document object model = > it is a tree like structure where all the elements are arranged in a hierarcal fashion
+// Dom enables to access the js to access the elements from the document so that we can easily manupulate them 
+
+
+// DOM - selectors:
+
+// getElementByTagName
+// getElementByClassName
+// getElementById
+//.queryselector
+//.qureyselectorall
+
+// var heading = window.document.getElementsByTagName("li");
+// console.log(heading)
+// var ex = document.getElementsByTagName("p")
+// console.log(ex)
+// var listofmobiles = document.getElementsByClassName("mobilelist")
+// console.log(listofmobiles
+
+// )
+
+// var specificbrand = document.getElementById("mobileBrands")
+// console.log(specificbrand)
+var randomelement = document.querySelector("h1")
+console.log(randomelement)
+var randomelements = document.querySelectorAll("h1")
+console.log(randomelements)
+
+
