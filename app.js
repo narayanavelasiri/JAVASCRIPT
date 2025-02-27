0ce3fd0770157cea0450cedba4c5556ef87b4699
narayanavelasiri <narayanavelasiri@gmail.com>
2025-02-27 22:10:48 +0530

    .

--- app.js
+++ app.js
@@ -10,20 +10,34 @@
 // .queryselector
 // .qureyselectorall
 
-var heading = window.document.getElementsByTagName("li");
-console.log(heading)
-var ex = document.getElementsByTagName("p")
-console.log(ex)
-var listofmobiles = document.getElementsByClassName("mobilelist")
-console.log(listofmobiles
+// var heading = window.document.getElementsByTagName("li");
+// console.log(heading)
+// var ex = document.getElementsByTagName("p")
+// console.log(ex)
+// var listofmobiles = document.getElementsByClassName("mobilelist")
+// console.log(listofmobiles
 
-)
+// )
 
-var specificbrand = document.getElementById("mobileBrands")
-console.log(specificbrand)
-var randomelement = document.querySelector("h1")
-console.log(randomelement)
-var randomelements = document.querySelectorAll("h1")
-console.log(randomelements)
+// var specificbrand = document.getElementById("mobileBrands")
+// console.log(specificbrand)
+// var randomelement = document.querySelector("h1")
+// console.log(randomelement)
+// var randomelements = document.querySelectorAll("h1")
+// console.log(randomelements)
+
+//adding the element for the existing dom tree
+var container = document.getElementById("container");
+container.innerText = "helloo"
+console.log(container)
+
+var htwo = document.getElementById("h2tag")
+htwo.appendChild(container)
 
 
+var newlistItem = document.createElement("li")
+newlistItem.textContent = "oneplus 10 prooo"
+newlistItem.setAttribute("id","oneplus")
+var mobileList = document.getElementById("mobileBrands")
+mobileList.prepend(newlistItem)
+//
